964ab3e6065731b42671093dba73fe285c144099
peter soleeh <petersoleeh@gmail.com>
2017-12-04 22:51:35 +0300

    class riot

---
+++ riot/mapper/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+import datetime as dt
+from django.contrib.auth.models import User
+
+
+# Create your models here.
+class Riot(models.Model):
+    location = models.CharField(max_length=255)
+    riot_dt = models.DateTimeField(auto_now_add=True)
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    magnitude = models.CharField(max_length=30)
