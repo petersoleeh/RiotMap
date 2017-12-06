68f3a6181345b93fa1ee9a56ede06740898ad938
mwerumuchai <mwerumuchai@gmail.com>
2017-12-06 12:20:39 +0300

    change database settings

--- riot/riot/settings.py
+++ riot/riot/settings.py
@@ -79,8 +79,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'riotmaps',
-        'USER': 'soleeh',
-        'PASSWORD':'soleeh..',
+        'USER': 'muchai',
+        'PASSWORD':'Ngea!2017##',
     }
 }
 
